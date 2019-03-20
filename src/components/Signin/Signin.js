d88566415526c5b59ed28d2b3aede8f415d95a2e
Tomas Vainoras <vainoras@gmail.com>
2019-03-20 19:31:30 +0200

    Update Signin.js

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ const Signin = ({onRouteChange}) => {
                 </fieldset>
                 <div className="">
                 <input 
-                onClick = {() =>  this.onRouteChange("home")}
+                onClick = {() =>  onRouteChange("home")}
                 className ="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
                 type="submit" 
                 value="Sign in"/>
